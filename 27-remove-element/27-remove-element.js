e01364d9877d48757a1abf3cc6cebebc2ecb6753
Hadeel Mahmoud <nez.hadeel@gmail.com>
2022-07-17 19:33:27 +0300

    Time: 91 ms (46.12%), Space: 45.1 MB (5.22%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+   let counter=0
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]==val){
+            nums[i]=null
+        }else counter++
+    }
+    nums.sort()
+    console.log(counter,nums,"nums")
+    return counter
+
+};
