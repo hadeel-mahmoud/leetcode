93aa42136610bd047d7093eb4276212eebf8f64b
Hadeel Mahmoud <nez.hadeel@gmail.com>
2022-11-13 00:04:35 +0200

    Time: 94 ms (59.74%), Space: 42.4 MB (37.18%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -1,13 +1,15 @@
-var rotate = function(M) {
-    let n = M.length, depth = ~~(n / 2)
-    for (let i = 0; i < depth; i++) {
-        let len = n - 2 * i - 1, opp = n - 1 - i
-        for (let j = 0; j < len; j++) {
-            let temp = M[i][i+j]
-            M[i][i+j] = M[opp-j][i]
-            M[opp-j][i] = M[opp][opp-j]
-            M[opp][opp-j] = M[i+j][opp]
-            M[i+j][opp] = temp
+var rotate = function(matrix) {
+    
+    for(let i=0;i<matrix.length;i++){
+        for(let j=i;j<matrix[i].length;j++){
+            let temp=matrix[j][i]
+            matrix[j][i]=matrix[i][j]
+            matrix[i][j]=temp
         }
+
+    }
+    
+ for (let i=0;i<matrix.length;i++) {
+        matrix[i].reverse()
     }
 };
