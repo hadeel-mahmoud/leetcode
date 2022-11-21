4c3293ed0b630b78d6cb289a0580fa31f2e8891a
Hadeel Mahmoud <nez.hadeel@gmail.com>
2022-11-22 00:05:48 +0200

    Time: 123 ms (27.80%), Space: 44.8 MB (19.00%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -3,36 +3,36 @@
     let nums2=[]
     
     num1 is a subset of nums 2
-               |
+               
     nums1 = [4,1,2]
-               |
-    nums1 = [1,3,4,2]
+               
+    nums2 = [1,3,4,2]
     
+    [1]
+    [1:3,3:4,4:null]
     output=[-1,3]
+    
  */
 
 var nextGreaterElement = function(nums1, nums2) {
     
-   let result=[]
-   
-   
-   for(let i=0;i<nums1.length;i++){
-       let nums2Index=nums2.findIndex((num)=>{ return num==nums1[i]})
-       
-       let counter=nums2Index+1
-       let found=false
-       while(counter<nums2.length){
-             if(nums2[counter]>nums2[nums2Index])
-             {
-                 result.push(nums2[counter])
-                 found=true
-                 break;
-             }
-           counter+=1
+   let map= new Map()
+   let stack=[]
+   let results=[]
+   for(let i=0;i<nums2.length;i++){
+       while(stack.length>0&&stack[stack.length-1]<nums2[i]){
+           map.set(stack.pop(),nums2[i])
        }
-       if(!found){
-           result.push(-1)
+       stack.push(nums2[i])  
+   }
+    
+   for(let i=0;i<nums1.length;i++){
+       let foundGreater=map.get(nums1[i])
+       if(foundGreater) {
+           results.push(foundGreater)
+       } else{
+          results.push(-1)
        }
    }
-    return result
+   return results
 };
