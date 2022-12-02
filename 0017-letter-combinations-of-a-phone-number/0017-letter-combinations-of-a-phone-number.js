452f58ed03a0622ecb0c2563adae69d0e8ede88b
Hadeel Mahmoud <nez.hadeel@gmail.com>
2022-12-02 22:52:52 +0200

    Time: 65 ms (88.71%), Space: 41.8 MB (84.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -1,29 +1,39 @@
-const letterCombinations = (digits) => {
-  if (digits == null || digits.length === 0) return [];
+/**/
 
-  const map = {
-    2: 'abc',
-    3: 'def',
-    4: 'ghi',
-    5: 'jkl',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz',
-  };
 
-  const res = [];
-  const go = (i, s) => {
-    if (i === digits.length) {
-      res.push(s);
-      return;
+const letterCombinations =(digits)=> {
+    
+    const output=[]
+    const map=new Map()
+    map.set('2',['a','b','c'])
+    map.set('3',['d','e','f'])
+    map.set('4',['g','h','i'])
+    map.set('5',['j','k','l'])
+    map.set('6',['m','n','o'])
+    map.set('7',['p','q','r','s'])
+    map.set('8',['t','u','v'])
+    map.set('9',['w','x','y','z'])
+    
+    const generateOutput=(str,index,substring)=>{
+    
+    if(index==str.length){
+        output.push(substring)
+        return;
+    }else{
+        let letters=map.get(str[index])
+        for(let i=0;i<letters.length;i++){
+           generateOutput(str,index+1,substring+letters[i])
+        }
     }
-
-    for (const c of map[digits[i]]) {
-      go(i + 1, s + c);
     }
-  };
+    
+    if(digits.length==0){
+       return output
+    }
+    generateOutput(digits,0,"")
+    
+    return output
 
-  go(0, '');
-  return res;
+    
 };
+
