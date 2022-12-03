184161e108f00a55ce152c1ed7446034aa1d30be
Hadeel Mahmoud <nez.hadeel@gmail.com>
2022-12-03 14:50:49 +0200

    Time: 1039 ms (16.19%), Space: 46.5 MB (56.19%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.js
@@ -0,0 +1,30 @@
+/*           
+    nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+    
+ */
+const fourSumCount = (nums1, nums2, nums3, nums4) =>{
+    let n=nums1.length
+    let tuples=0
+    let sums={}
+    for(let i=0;i<n;i++)
+    {
+         for(let j=0;j<n;j++)
+         {
+             if( sums[nums1[i]+nums2[j]]){
+                 sums[nums1[i]+nums2[j]]+=1
+             }else{
+                 sums[nums1[i]+nums2[j]]=1 
+             }
+         }
+    }
+
+    for(let i=0;i<n;i++)
+    {
+         for(let j=0;j<n;j++)
+         {
+           if(sums[-nums3[i]-nums4[j]])
+           tuples+=sums[-nums3[i]-nums4[j]]
+         }
+    }
+    return tuples
+};
